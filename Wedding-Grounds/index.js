0125c75c4d3a4219aac0ca1eb9e6a9168abfb448
ruthhawino <awinoruthh@gmail.com>
2023-06-12 14:41:46 +0300

    gallery update

---
+++ Wedding-Grounds/index.js
@@ -0,0 +1,33 @@
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+$(document).ready(function() {
+    $(".Events .ev1").hover(
+      function() {
+        $(this).find(".hover-text").text("AVAILABLE 24 HOURS RATED AT 4 STARS TRY NOW");
+      },
+      function() {
+        $(this).find(".hover-text").text("");
+      }
+    );
+  
+    $(".Events .ev2").hover(
+      function() {
+        $(this).find(".hover-text").text("AVAILABLE 24 HOURS RATED AT 4 STARS TRY NOW");
+      },
+      function() {
+        $(this).find(".hover-text").text("");
+      }
+    );
+  
+    $(".Events .ev3").hover(
+      function() {
+        $(this).find(".hover-text").text("AVAILABLE 24 HOURS RATED AT 4 STARS TRY NOW");
+      },
+      function() {
+        $(this).find(".hover-text").text("");
+      }
+    );
+  });
+  
